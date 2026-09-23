59a96e0531c799c7730a6f211cdcd767f4aca6da
Pepe Cruz <jcruz_garciah@hotmail.com>
2026-09-22 21:57:47 -0600

    build(css): import index.css from entry module instead of html link

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/src/App";
 import { AuthSProvider } from "./components/src/contexts/AuthContext";
 import { ThemeProvider } from "./components/src/contexts/ThemeContext";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
